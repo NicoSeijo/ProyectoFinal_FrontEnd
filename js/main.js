98962d257ba8c0c4a9c428478344ea81b8f81ebf
NicoSeijo <103322629+NicoSeijo@users.noreply.github.com>
2022-12-02 14:48:26 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const sliderTema = document.querySelector('#switch input[type="checkbox"]');
+/*const sliderTema = document.querySelector('#switch input[type="checkbox"]');
             function cambiaTema(ev){
                 if(ev.target.checked){
                     document.documentElement.setAttribute('tema', 'dark');
@@ -6,4 +6,19 @@ const sliderTema = document.querySelector('#switch input[type="checkbox"]');
                     document.documentElement.setAttribute('tema', 'light');
                 }
             }
-            sliderTema.addEventListener('change', cambiaTema);
+            sliderTema.addEventListener('change', cambiaTema);*/
+
+const preferedColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+const slider = document.getElementById('botonTema');
+
+const setTheme = (theme) => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+}
+
+slider.addEventListener('click', ()  => {
+    let switchToTheme = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+    setTheme(switchToTheme);
+});
+
+setTheme(localStorage.getItem('theme') || preferedColorScheme);
